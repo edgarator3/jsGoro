650954ae6d36aa4385210e6a516de6c3d89e597c
Ed <memory-forever@mail.ru>
2023-06-08 22:47:37 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let x = 2;
+// x = [3, 7, 9, 0, 44, 56, 4 < 13];
+// let y;
+// y = [44, 77, 88, 99, 109, 123, 11];
+// console.log(y[y.length - 2] - y[y.length - 1]);
+// let x=2;
+console.log(x ** 3);
